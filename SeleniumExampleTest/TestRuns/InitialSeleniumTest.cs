e9f7881d6c33f89acb2b24c3bb9a658c67e5d1fb
George Okello <gokello26@gmail.com>
2022-01-02 03:17:37 -0600

    Clear old project files and start new.

--- SeleniumExampleTest/TestRuns/InitialSeleniumTest.cs
+++ SeleniumExampleTest/TestRuns/InitialSeleniumTest.cs
@@ -1,13 +1,33 @@
 using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
 
 namespace SeleniumExampleTest.TestRuns;
 
 [TestFixture]
 public class InitialSeleniumTest
 {
+    private IWebDriver _driver;
+    
+    [SetUp]
+    public void Initialize()
+    {
+        _driver = new ChromeDriver();
+    }
+    
     [Test]
     public void TestMethod1()
     {
+        _driver.Navigate().GoToUrl("https://youtube.com");
         Console.WriteLine("TestMethod1");
+        Thread.Sleep(5000);
+    }
+    
+    [TearDown]
+    public void CleanUp()
+    {
+        _driver.Quit();
+        _driver.Dispose();
     }
 }
